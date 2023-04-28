945751e376f7dd5a3ad5f3c2a3d7b467d3a152bc
James Grom <jgrom@scu.edu>
2023-04-28 12:44:35 -0700

    cursory implementation of authillo class

--- src/index.ts
+++ src/index.ts
@@ -1,116 +1,35 @@
+/**
+ * @param {string} clientId - Unique identifier of your app - for a full explanation of this parameter, visit https://authillo.com/docs/clientId
+ * @param {string} defaultRedirectUri - The default url that the user will be redirected back to (on your site/app) after completing the authorization flow. Must exactly match one of the url's provided in your platform configuration. - for a full explanation of this parameter, visit https://authillo.com/docs/defaultRedirectUri
+ * @param {boolean} [enforceStrictSecurity=false] - Flag, which if set, will require the code_verifier flow - for a full explanation of this parameter, visit https://authillo.com/docs/enforceStrictSecurity
+ * @param {()=>Promise<string>} [generateCodeChallenge] - Function that handles generating the code challenge by requesting a code challenge from your backend- (if this function is not provided, we use a default code challenge which is the same across all users, effectively skipping the code verification part of the oidc flow) - for a full explanation of this parameter, visit https://authillo.com/docs/generateCodeChallenge
+ */
 export type config = {
 	clientId: string;
-	clientSecret: string;
+	defaultRedirectUri: string;
+	enforceStrictSecurity?: boolean;
+	generateCodeChallenge?: () => Promise<string>;
 };
-export type Attribute =
-	| "name"
-	| "age"
-	| "face"
-	| "selfie"
-	| "idphoto"
-	| "email"
-	| "phone"
-	| "license"
-	| "sex"
-	| "birthdate"
-	| "linkedin"
-	| "google"
-	| "instagram";
-type result = {
-	user?: {
-		sub: string;
-		name?: string;
-		age?: number;
-		birthdate?: string;
-		phoneNumber?: string;
-		sex?: "MALE" | "FEMALE";
-		email?: string;
-		riskScore?: number; //deprecated
-		phoneNumberData?: {
-			type?: string;
-		};
-		faceInfo?: {
-			faceVerified: boolean;
-			faceVerificationMR: boolean;
-			riskScore: number;
-		};
-		idPhotoInfo?: {
-			hasLicense: boolean;
-			parsedLicenseData?: {
-				licenseNumber: string;
-				firstName: string;
-				lastName: string;
-				idType: string;
-				expirationDate: string;
-				birthdate: string;
-				state: string;
-				zipCode: string;
-				manuallyReviewed: Boolean;
-			};
-			licenseImageFront?: string;
-			licenseImageBack?: string;
-		};
-		licenseInfo?: {
-			hasLicense: boolean;
-			parsedLicenseData?: {
-				licenseNumber: string;
-				firstName: string;
-				lastName: string;
-				idType: string;
-				expirationDate: string;
-				birthdate: string;
-				state: string;
-				zipCode: string;
-				manuallyReviewed: Boolean;
-			};
-		};
-		linkedinInfo?: {
-			linkedinVerified: boolean;
-			linkedinData?: {
-				linkedinId: string;
-				name: string;
-				firstName: string;
-				lastName: string;
-				email: string;
-				emailVerified: boolean;
-				picture?: string;
-				locale?: string;
-			};
-		};
-	};
-};
-
-export class authillo {
-	private clientId: string;
-	private clientSecret: string;
-	constructor(config: config) {
+class authillo {
+	private clientId?: string;
+	private defaultRedirectUri?: string;
+	private enforceStrictSecurity: boolean = false;
+	private codeVerifier?: string;
+	constructor() {}
+	public initialize(config: config) {
 		this.clientId = config.clientId;
-		this.clientSecret = config.clientSecret;
+		this.defaultRedirectUri = config.defaultRedirectUri;
+		if (config.enforceStrictSecurity != null)
+			this.enforceStrictSecurity = config.enforceStrictSecurity;
+		if (config.generateCodeChallenge != null)
+			this._generateCodeChallenge = config.generateCodeChallenge;
 	}
-	verify: (
-		user_phone_number: string,
-		attributes: Attribute[]
-	) => Promise<{ userStatus: "pending" | "verified" }> = async (
-		user_phone_number: string,
-		attributes: Attribute[]
-	) => {
-		return { userStatus: "pending" };
-	};
-
-	getVerifiedInfo: (user_phone_number: string, attributes: Attribute[]) => Promise<result> = async (
-		user_phone_number: string,
-		attributes: Attribute[]
-	) => {
-		return {
-			user: {
-				sub: "asdflkivnwi29040nsfl1i38fncsk10234uls10csdnf0s810cn",
-				email: "james@authillo.com",
-				faceInfo: {
-					faceVerified: true,
-					riskScore: 0,
-					faceVerificationMR: true,
-				},
-			},
-		};
+	private initializationIsValid() {
+		return this.clientId != null && this.defaultRedirectUri != null;
+	}
+	private _generateCodeChallenge = () => {
+		//
 	};
 }
+
+export const Authillo = new authillo();
